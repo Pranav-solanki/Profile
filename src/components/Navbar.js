a485bd95d0fcce1aedf7adf19020da6da28802e1
Pranav solanki <github39645@gmail.com>
2025-09-12 10:57:36 +0530

    navbar going on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,25 @@
-import { useState } from "react";
+import "./Body.css";
 import "./Navbar.css";
 export default function Navbar() {
-  const [darkmode, setdarkmode] = useState(false);
-  function handleclick(id) {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  }
-  function togglemode(){
-    document.body.classList.toggle('dark-mode')
-  }
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-logo">My portfolio</div>
-        <ul>
-          <li onClick={handleclick("home")}>Home</li>
-          <li onClick={handleclick("about")}>About</li>
-          <li onClick={handleclick("project")}>Projects</li>
-          <li onClick={handleclick("contact")}>Contact</li>
-          <li>
-            <a href="/resume.pdf" target="_blank">
-              Resume
-            </a>
-          </li>
-        </ul>
-        <div className="theme">
-          <button onClick={togglemode}>change mode</button>
+      <div className="container">
+        <div className="logo">I am logo</div>
+        <div className="right-side">
+          <div className="headings">
+            <nav>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          </div>
+          <div className="mode"><i class="fa-solid fa-moon"></i></div>
         </div>
-      </nav>
+      </div>
     </>
   );
 }
