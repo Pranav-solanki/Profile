4e9f8f2794e09b1bf5ae74ecc1bc2f4bb0ce11ec
Pranav solanki <github39645@gmail.com>
2025-10-04 12:04:44 +0530

    project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,42 @@
 import React from "react";
+import "./Project.css";
 
-export default function Home() {
+export default function Projects() {
+  const projectList = [
+    {
+      title: "Portfolio Website",
+      description:
+        "A modern and responsive personal portfolio built with React.",
+      link: "",
+    },
+    {
+      title: "ToDo App",
+      description:
+        "A full-featured task manager with CRUD operations using React and LocalStorage.",
+      link: "https://pranav-solanki.github.io/Todo-List/",
+    },
+    {
+      title: "News App",
+      description:
+        "A News forecast app that fetches real-time data from OpenNewsApp API.",
+      link: "https://pranavstech.me",
+    },
+  ];
+
   return (
     <div id="Projects">
-      <h1>I am Projects section</h1>
+      <h2 className="projects-title">Projects</h2>
+      <div className="projects-grid">
+        {projectList.map((project, index) => (
+          <div className="project-card" key={index}>
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <a target="_blank" href={`${project.link}`}>
+              go
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
